e89412d0cbb7d09a58479b5f0127f16a8a3f6264
ramathipparthy <deviacpt@gmail.com>
2023-09-04 14:20:19 +0530

    commit message

---
+++ src/test/java/pack1/Test1.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test
+	public void m1()
+	{
+		System.out.println("welcome to selenium automation testing");
+	}
+
+}
---
+++ src/test/java/pack1/Test2.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+	public void m2()
+	{
+		System.out.println("welcome to developers");
+	}
+
+}
